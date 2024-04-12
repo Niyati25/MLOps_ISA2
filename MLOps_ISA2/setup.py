b91632764c9205840983441c14335643db1932fa
Giselle06 <101310332+giselle06@users.noreply.github.com>
2024-04-12 10:14:03 +0530

    Cookiecutter

---
+++ MLOps_ISA2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOps Lab Practical Isa2',
+    author='Niyati 2312, Giselle 2308',
+    license='MIT',
+)
